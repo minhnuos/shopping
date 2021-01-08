5d56d27da6d9079d31a8aa5e0598d912bedfb603
MinhNV <nguyenvanminhhy99@gmail.com>
2021-01-08 11:28:30 +0700

    one7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        URL::forceScheme('https');
         Schema::defaultStringLength(191);
     }
 }
